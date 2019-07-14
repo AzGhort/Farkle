0b4e17f523b7d01ee5c7fba42bf6bd6747d1fa37
AzGhort <ghort@seznam.cz>
2019-07-14 23:57:48 +0200

    initial

--- Farkle/Program.cs
+++ Farkle/Program.cs
@@ -6,9 +6,7 @@ namespace Farkle
     {
         static void Main(string[] args)
         {
-            var state = new GameState();
-            var combs = state.GetAllDiceCombinations();
-            int a = 2;
+            
         }
     }
 }
