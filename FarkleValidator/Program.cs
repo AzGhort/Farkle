88d0f98f1697aadd6ce3aea0a2d7ff76c669d5c6
AzGhort <ghort@seznam.cz>
2019-07-22 16:49:54 +0200

    Validator folder

---
+++ FarkleValidator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FarkleValidator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
